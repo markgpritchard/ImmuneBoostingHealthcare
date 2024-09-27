85a9fcb57e808e43e66a740d7a6fe85a6a626a0c
markgpritchard <62439333+markgpritchard@users.noreply.github.com>
2024-09-27 14:43:29 +0100

    Update gatheroutputs.sh

--- scripts/gatheroutputs.sh
+++ scripts/gatheroutputs.sh
@@ -1,17 +1,17 @@
 #! /bin/bash
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=3
+#SBATCH --ntasks-per-node=4
 #SBATCH --time=12:00:00
 #SBATCH --partition=short
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=mark.pritchard@ndm.ox.ac.uk
 
-export JULIA_NUM_THREADS=1
+export JULIA_NUM_THREADS=4
 module load Julia/1.9.3-linux-x86_64
 
 for n in {1..3}
 do
-        julia scripts/gatheroutputs.jl "$n" &
+        julia scripts/gatheroutputs.jl "$n"
 done
 
 wait
