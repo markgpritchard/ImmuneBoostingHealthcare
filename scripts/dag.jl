243c84c8486a17eb9e024d916c019b39221b7dca
markgpritchard <62439333+markgpritchard@users.noreply.github.com>
2024-11-11 21:03:00 +0000

    update dag

--- scripts/dag.jl
+++ scripts/dag.jl
@@ -7,7 +7,7 @@ using CairoMakie, MakieTeX
 
 healthcaredag = let 
     td1 = TeXDocument(read(scriptsdir("dag.tex"), String))
-    fig = Figure(; size=( 500, 300 ))
+    fig = Figure(; size=( 500, 400 ))
     ga = GridLayout(fig[1, 1:2])
     lt1 = LTeX(ga[1, 1], td1; tellwidth=false)
 
